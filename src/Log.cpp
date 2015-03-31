f1022415fdde0f70f3e449bc2a6087a35c4bd60a
Anthony Wertz <awertz@cmu.edu>
2015-03-31 16:28:50 -0400

    Removed files in different repos.

--- src/Log.cpp
+++
@@ -1,9 +0,0 @@
-#include "srl/Log.hpp"
-
-namespace al { namespace srl
-{
-    const std::vector<Log::LogLevel> Log::AllLevels =
-        { Log::Info, Log::Warning, Log::Error, Log::Debug };
-    const std::vector<Log::LogLevel> Log::NotErrors =
-        { Log::Info, Log::Warning, Log::Debug };
-} }
